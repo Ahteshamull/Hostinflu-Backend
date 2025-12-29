be93101032c6a36d8bf03387ba9a1a2fb96914b7
ahteshamulhasan <ahteshamulhasan2@gmail.com>
2025-12-29 22:52:55 +0600

    faq add done

--- src/legalDoc/schema/legalDoc.modal.js
+++ src/legalDoc/schema/legalDoc.modal.js
@@ -5,7 +5,7 @@ const legalDocSchema = new Schema(
   {
     content: {
       type: String,
-      enum: ["termsAndCondition", "privacyPolicy", "aboutUs"],
+      enum: ["termsAndCondition", "privacyPolicy", "aboutUs", "faq"],
       required: [true, "Content type is required"],
     },
     description: {
