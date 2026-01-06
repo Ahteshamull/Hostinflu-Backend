388036fcc3de5437901cdb1cd1e90f5aabaf6641
ahteshamulhasan <ahteshamulhasan2@gmail.com>
2026-01-07 01:37:07 +0600

    update

--- src/collaboration/controller/collaboration.controller.js
+++ src/collaboration/controller/collaboration.controller.js
@@ -42,9 +42,9 @@ export const createCollaboration = async (req, res) => {
 
  
     if (userRole === "host") {
-   
+   //this is for host
     } else if (userRole === "influencer") {
-     
+     //this is for influencer
     }
 
     const newCollaboration = new Collaborations({
