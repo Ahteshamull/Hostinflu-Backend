46f3d818dc7a95c82b54938d4fefc11ea91bb766
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2026-01-09 21:53:24 +0600

    update one

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import cors from "cors";
 import { initializeSocket } from "./socket/connection/socket.Connection.js";
 
 const app = express();
+
+//socket server
 const server = createServer(app);
 
 const PORT = process.env.PORT || 5000;
