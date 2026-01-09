cae86c89fedc8b482e6fce30032100dd08d3ce0c
“eusouf12” <riyajuddeneusouf@gmail.com>
2026-01-10 00:29:59 +0600

    Update uploads picture and solve the error issue

--- src/index.js
+++ src/index.js
@@ -3,17 +3,26 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import { createServer } from "http";
 import cookieParser from "cookie-parser";
 import dbConnect from "./config/database/dbConfig.js";
 import router from "./api/index.js";
 import cors from "cors";
+import { initializeSocket } from "./socket/connection/socket.Connection.js";
+
 const app = express();
+
+//socket server
 const server = createServer(app);
 
 const PORT = process.env.PORT || 5000;
 
+// Initialize Socket.IO
+initializeSocket(server);
+
 app.use(
-  cors({ origin: "*", // Adjust this to your frontend URL in production
+  cors({
+    origin: "*", // Adjust this to your frontend URL in production
     credentials: true,
   })
 );
@@ -40,6 +49,7 @@ app.get("/", (req, res) => {
 dbConnect();
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`✅ Server running at ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`🛜  Server running at ${PORT}`);
+  console.log(`⚡ Socket.IO server started`);
 });
