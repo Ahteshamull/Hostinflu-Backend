af1d34cc2dc452770b6639c594ba91ee3c061807
ahteshamul hasan <ahteshamulhasan2@gmail.com>
2026-01-14 03:40:17 +0600

    update

--- src/admin/routes/admin.route.js
+++ src/admin/routes/admin.route.js
@@ -35,7 +35,7 @@ router.put(
 );
 
 //localhost:3000/api/v1/admin/delete-admin/:id
-router.delete("/delete-admin/:id", superAdminMiddleware, deleteAdmin);
+router.delete("/delete-admin/:id", authenticateToken, deleteAdmin);
 
 //localhost:3000/api/v1/admin/all-admins
 router.get("/all-admins", authenticateToken, allAdmin);
