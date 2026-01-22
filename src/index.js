00eb7804b518ec765742c8559f5098300a8c785e
ahteshamul hasan <ahteshamulhasan2@gmail.com>
2026-01-23 04:18:27 +0600

    Update index file

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ app.get("/", (req, res) => {
 
 dbConnect();
 
-// Start server
+
 server.listen(PORT, () => {
   console.log(`🛜  Server running at ${PORT}`);
   console.log(`⚡ Socket.IO server started`);
