7250b1b0ef7c3b7af3c719cb9c9a50343dbfe0dc
ahteshamul hasan <ahteshamulhasan2@gmail.com>
2026-02-02 04:06:23 +0600

    add

--- src/payment/controller/payment.controller.js
+++ src/payment/controller/payment.controller.js
@@ -10,7 +10,6 @@ const stripe = process.env.STRIPE_SECRET_KEY
   ? new Stripe(process.env.STRIPE_SECRET_KEY)
   : null;
 
-// stripe account onboarding
 export const stripeAccountOnboarding = async (req, res) => {
   try {
     const userId = req.user?.id || req.user?._id || req.user?.userId;
