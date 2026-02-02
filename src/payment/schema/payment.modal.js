24d0f3fb5d0678a6f96dc3424cdf13bf7cdf583c
ahteshamul hasan <ahteshamulhasan2@gmail.com>
2026-02-03 01:16:21 +0600

    update payment status

--- src/payment/schema/payment.modal.js
+++ src/payment/schema/payment.modal.js
@@ -27,7 +27,13 @@ const PaymentSchema = new Schema(
     },
     status: {
       type: String,
-      enum: ["PENDING", "SUCCESS", "IN_PROGRESS", "FAILED", "CANCELLED"], // adjust to your PaymentStatus enum
+      enum: [
+        "PENDING",
+        "SUCCESS",
+        "IN_PROGRESS || HOLD",
+        "FAILED",
+        "CANCELLED",
+      ], // adjust to your PaymentStatus enum
       required: true,
     },
     provider: {
