7d9021c6c79a1322ff2fd99a1ce3127097c52ac4
ahteshamul hasan <ahteshamulhasan2@gmail.com>
2026-02-04 02:39:24 +0600

    all review retrev success fully

--- src/review/routes/review.route.js
+++ src/review/routes/review.route.js
@@ -2,9 +2,9 @@ import express from "express";
 import {
   createReview,
   userPersonalReview,
-  getReview,
-  updateReview,
+  userReview,
   deleteReview,
+  allReviews,
 } from "../controller/review.controller.js";
 import { authenticateToken } from "../../helper/middlewares/auth.middleware.js";
 
@@ -13,16 +13,16 @@ const router = express.Router();
 // localhost:3000/api/v1/review/create-review/:collaborationId
 router.post("/create-review/:collaborationId", authenticateToken, createReview);
 
-// localhost:3000/api/v1/review/user-personal
+// http://localhost:3000/api/v1/review/user-personal
 router.get("/user-personal", authenticateToken, userPersonalReview);
 
-// localhost:3000/api/v1/review/get
-router.get("/get", authenticateToken, getReview);
+// localhost:3000/api/v1/review/user/:userId
+router.get("/user/:userId", authenticateToken, userReview);
 
-// localhost:3000/api/v1/review/update
-router.put("/update", authenticateToken, updateReview);
+// localhost:3000/api/v1/review/delete/:reviewId
+router.delete("/delete/:reviewId", authenticateToken, deleteReview);
 
-// localhost:3000/api/v1/review/delete
-router.delete("/delete", authenticateToken, deleteReview);
+// localhost:3000/api/v1/review/all-reviews
+router.get("/all-reviews", authenticateToken, allReviews);
 
 export default router;
